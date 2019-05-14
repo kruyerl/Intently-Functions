7b47e867de539766342f7cb41edcfd97f56e997d
Luke Kruyer <kruyerl@gmail.com>
2019-05-14 23:59:45 +0200

    about to add cors middleware

--- functions/handlers/data.js
+++ functions/handlers/data.js
@@ -5,10 +5,59 @@ const firebase = require('firebase')
     type: GET
     no body required
  */
-exports.syncDataDownstream = (req, res) => {}
 
-/**
- *  !Sync Data Upstream
-    type: Post
- */
-exports.syncDataUpstream = (req, res) => {}
+
+const getActions = function(req) {
+  return new Promise(((resolve, reject) => {
+    db.collection(`/users/${req.user.username}/actions`)
+      .orderBy("createdAt", "desc")
+      .get()
+      .then(data => {
+        const actions = [];
+        data.forEach(doc => {
+          actions.push({
+            actionId: doc.id,
+            body: doc.data().body,
+            createdAt: doc.data().createdAt
+          });
+        });
+        return actions;
+      })
+      .catch(error => console.error(error));
+  }));
+}
+const getHabits = function(req) {
+  return new Promise((resolve, reject) => {
+    db.collection(`/users/${req.user.username}/habits`)
+      .orderBy("createdAt", "desc")
+      .get()
+      .then(data => {
+        const habits = [];
+        data.forEach(doc => {
+          habits.push({
+            habitId: doc.id,
+            body: doc.data().body,
+            createdAt: doc.data().createdAt
+          });
+        });
+        return habits ;
+      })
+      .catch(error => console.error(error));
+  });
+};
+
+
+
+exports.syncDataDownstream = (req, res) => {
+    const allData = {}
+    Promise.all([getActions(req), getHabits(req)])
+      .then(res => {
+        console.log(res);
+        return res.json({res});
+      })
+      .catch(err => {
+        console.error(err);
+      });
+}
+
+
--- functions/index.js
+++ functions/index.js
@@ -44,29 +44,28 @@ app.post('/signup', signUp)
 app.post('/login', logIn)
 app.post('/reset', resetPassword)
 app.get("/user", FBAuth, getUser)
-app.patch("/user/update", FBAuth, patchUser)
+app.post("/user/update", FBAuth, patchUser)
 
 //  Actions Routes
 app.get('/actions', FBAuth, getActions)
 app.post('/actions', FBAuth, postAction)
-app.patch('/actions', FBAuth, patchAction)
-app.delete('/actions', FBAuth, deleteAction)
+app.post('/actions/update', FBAuth, patchAction)
+app.post('/actions/delete', FBAuth, deleteAction)
 
 //  Objectives Routes
 app.get("/objectives", FBAuth, getObjectives)
 app.post("/objectives", FBAuth, postObjective)
-app.patch("/objectives", FBAuth, patchObjective)
-app.delete("/objectives", FBAuth, deleteObjective)
+app.post("/objectives/update", FBAuth, patchObjective)
+app.post("/objectives/delete", FBAuth, deleteObjective)
 
 //  Habits Routes
 app.get("/habits", FBAuth, getHabits)
 app.post("/habits", FBAuth, postHabit)
-app.patch("/habits", FBAuth, patchHabit)
-app.delete("/habits", FBAuth, deleteHabit)
+app.post("/habits/update", FBAuth, patchHabit)
+app.post("/habits/delete", FBAuth, deleteHabit)
 
 //  Data Sync
-app.get("/habits", FBAuth, syncDataDownstream);
-app.post("/habits", FBAuth, syncDataUpstream);
+app.get("/data", FBAuth, syncDataDownstream);
 //  TODO getAllData
 
 
